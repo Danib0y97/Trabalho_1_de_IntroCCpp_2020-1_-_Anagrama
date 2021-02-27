5136571eeb9e14a38fbbb7b85048a2d8db3c7765
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-02-26 23:25:29 -0300

    Falta caracteres e não repetição
    Estamos quase acabando, falta apenas corrigir o problema de caracter e da não repetição de anagramas.

--- pseud/trab/main.c
+++ pseud/trab/main.c
@@ -3,182 +3,180 @@
 #include <string.h>
 #include <time.h>
 #include <locale.h>
+#include <ctype.h>
 
-#define TAM_LISTA 9
-
-int menu (inicio) {
- 
- int intro, nivel;
-
-  printf("///////Seja bem-vindo a ANAGRAMA!///////\n");
-
-  printf("0.Sair\n1.Iniciar\n");
-  scanf("%d", &intro);
-  if ( intro == 1){
-    printf("Escolha um nível para jogar:\n1.Fácil\n2.Médio\n3.Difícil\n");
-    scanf("%d", &nivel);
-
-    switch (nivel){;
-    case 1:
-      printf("Voce escolheu o nível %d - Fácil\n", nivel);
-      break;
-    case 2:
-      printf("Voce escolheu o nível %d - Médio\n", nivel);
-      break;
-    case 3:
-       printf("Voce escolheu o nível %d - Difícil\n", nivel);
-       break;
-    }   
- }else {
-     printf("Tchau amigo, até à próxima");
-  }
-  return 0;
-}
-
+#define TAM_LISTA 20
 
 
 int main(){
-  
+
   /*Contadores de palavras*/
-  int a, i = 0, j = 0, c = 0, x = 0, score = 0, decisao;  
+  int i = 0, j = 0, c , x , n = 0, score = 0, decisao, tam_resposta, tam_anagrama;
   /*Arquivo*/
-  FILE *lista;
-  /* Palavras da lista*/
-  char palavras[20], palavras2[20], anagrama[20], r[20], resposta[20]; 
+  FILE *lista;  
+  /*Palavras da lista*/
+  char palavra[20], palavraslista[20], anagrama[20], r[20], resposta[20], * comparar; //palavras_lidas[0];
   /*Sorteia a palavra aleatória da lista*/
-  int random = 0, sorteio = 0, nivel;
+  int sorteio = 0, nivel;
+ 
+  printf("//////////// Seja bem-vindo a ANAGRAMA! ////////////\n\n");
 
-  a = 0;
-  while (a <= TAM_LISTA){
-    menu(a);
-    /* função do menu */
+  do{
+    printf("\n1.Iniciar\n2.Como jogar?\n3.Sair\n\n");
+    scanf("%d", &decisao);
   
-    /*Abrindo o arquivo e lendo as palavras*/
-      if (nivel == 1){
-        lista = fopen("facil.txt", "r");
-        if(lista == NULL){
-            printf("O arquivo não pode ser aberto.\n");
-            exit(1);
+    switch(decisao){
+        
+      case 1:
+        printf("\nPronto?! Vamos lá!\n\n");
+        break;
+      case 2:
+        printf("\nO objetivo desse jogo e formar uma palavra com as letras que serao apresentadas na tela.\n");
+        printf("\n1.Voltar\n2.Sair\n\n");
+        scanf("%d", &decisao);
+        if(decisao == 1){
+          continue;
         }
+        else if(decisao == 2){
+          return 0;
+        }
+        else{
+          printf("\nEscolha entre umas das opcoes!\n\n");
+          continue;
+        }   
+      case 3:
+        printf("\nTchau, amigo! Ate a proxima!\n\n");
+        return 0;
+      default:
+        printf("\nEscolha entre umas das opcoes!\n\n"); 
+        continue;
       }
-       if (nivel == 2){
-        lista = fopen("medio.txt", "r");
-        if(lista == NULL){
-            printf("O arquivo não pode ser aberto.\n");
-            exit(1);
-        }
-       }
-        if (nivel == 3){
-        lista = fopen("dificil.txt", "r");
-        if(lista == NULL){
-            printf("O arquivo não pode ser aberto.\n");
-            exit(1);
-        } 
-      }  
-
+    break;
+  } while(1);
   
-    //lista = fopen("palavras.txt", "r");
-   // if(lista == NULL){
-     // printf("O arquivo não pode ser aberto.\n");
-     // exit(1);
-    ///}
-    while(fscanf( lista, "%s", palavras) != EOF ){
-      random++;
-    }
+  while(1){
+  /*Abrindo o arquivo e lendo as palavras*/
 
+    lista = fopen("facil.txt","r");
+    if(lista == NULL) {
+      printf("O arquivo nao pode ser aberto.\n");
+      exit(1);
+    }
+    rewind(lista);
+    
     /*Sorteando palavras da lista*/
 
-    while(1){
+    while(fscanf(lista, "%s", palavra) != EOF){
+      //strcpy(palavraslista, palavra);
       rewind(lista);
-    
+
       srand(time(NULL));
-      sorteio = rand() % TAM_LISTA;  
+      sorteio = rand() % TAM_LISTA;
 
       for(i = 0, j = 0; j < sorteio; i++){
         if(fgetc(lista) == '\n'){
           j++;
-          fscanf(lista, "%s", palavras);
-        } 
+          fscanf(lista, "%s", palavra);
+        }
       }
-      score = score + 10;
       break;
     }
-    fclose(lista);
-    printf("%s\n", palavras);
-  
-    /*Copiando a palavra*/
-    strcpy(palavras2, palavras);
-    /*printf("Este é o strcpy: %s\n", palavras2); */   
-  
-  
-    /*Embaralhando a palavra*/
-  
-    for(i = 0; i < strlen(palavras); i++){
+    printf("%s\n", palavra);
 
-      x = rand() % strlen(palavras);
+
+    /*Embaralhando a palavra*/    /*voltar*/
+
+    for(i = 0; i < strlen(palavra); i++){
+
+      x = rand() % strlen(palavra);
       c = 0;
-      if (i == 0){
-        anagrama[i] = palavras[x];
+      if (i == 0) {
+        anagrama[i] = palavra[x];
         r[i] = x;
       }
-      else{
-        for(j = 0; j < i; j++){
-          if(r[j] != x){
+      else {
+        for(j = 0; j < i; j++) {
+          if(r[j] != x) {
             c++;
-        }
-          else{
-            x = rand()%strlen(palavras);
+          }
+          else {
+            x = rand() % strlen(palavra);
             c = 0;
-            j= -1;
-        
-          
-        }
-          if(c == i){
-            anagrama[i] = palavras[x];
+            j = -1;
+          }
+          if(c == i) {
+            anagrama[i] = palavra[x];
             r[i] = x;
-        }
-      }    
+          }
+        }  
+      }
     }
-  }
-    printf("\nResponda com letra maiuscula!\n\n");
     printf("%s\n\n", anagrama);
-  
-   /*Resposta*/
+
+
+  /*//////////Resposta//////////*/
+    
     scanf("%s", resposta);
-    if(strcmp (resposta, palavras) == 0){
-   /*Fazer uma lista de frases!*/
-      printf("\nAcertou!\n\nSCORE: %d\n\nDeseja jogar mais?\n\n", score);
-      printf("1.SIM!\n2.NAO!\n\n");
-      scanf("%d", &decisao);
-      
-      if(decisao == 2){
-        printf("\nPoxa! Voce nao pode desistir agora, tenha determinacao!\n\nDeseja realmente sair?\n\n");
-        printf("1.SIM! ME DEIXA!\n2.NAO! Eu quero ficar mais um pouquinho!\n\n");
-        scanf("%d", &decisao);
-        
-        if(decisao == 1){
-          printf("\nOk, entao!\nAte logo! :c\n\n");
-          return 0;
-        }
-        else if(decisao == 2){
-          printf("\nTe amo! <3\n\n");
-        }
-      }
-      else if(decisao == 1){
-        printf("\nBrabo(a) tem nome!\n\n");
-      }
+
+  /*Tranformando em letra maiúscula caso precise*/
+    
+    tam_resposta = strlen(resposta);
+    for(i = 0; i < tam_resposta; i++){
+      resposta[i] = toupper(resposta[i]);
     }
-    else{
-      printf("\nNao foi dessa vez! Tentar novamente?\n\n");
-      printf("1.SIM!\n2.NAO!\n\n");
+
+
+  /*Randomizar a palavra*/
+    
+    if(strcmp(resposta,"RANDOM") == 0){
+      continue;
+    }
+
+  /*Condição de parada*/
+    
+    if(strcmp(resposta,"FIM")==0){
+      printf("Obrigada por jogar! Até mais!");
+      return 0;
+    }
+
+   
+  //////////////////* Não repetindo a palavra *////////////////////////
+
+   /* for(i = 0; i < strlen(palavra); i++){
+         n  =  palavras_lidas[0];
+         palavras_lidas[0] = palavras_lidas[1];
+         palavras_lidas[1] = palavras_lidas[0];
+    } */
+  
       
-      if(decisao == 2){
-        break;
+    //Comparando tamanho e com palavras da lista
+    
+    tam_anagrama = strlen(palavra);
+    if(tam_resposta != tam_anagrama){
+      printf("\nNao e um anagrama! Tente novamente!\n");
+      continue;
+    }
+    
+    rewind(lista); /////NAO TIRAR!
+    while(fgets(palavraslista, sizeof(palavraslista), lista)){  
+        comparar = strstr(palavraslista, resposta);
+        if (comparar){
+          break;
+        }
       }
-      else if(decisao == 1){
-      }
-    } 
-    continue;
-  }
+      if (comparar){
+          score = score + 10;
+          printf("É anagrama!\n\n");
+          printf("SCORE: %d\n\n", score);
+          scanf("%d", &decisao);
+          continue;
+        }
+        else{
+          printf("Nao e anagrama!\n\n");
+          printf("SCORE: %d\n\n", score);
+          continue;
+        }     
+      }      
+  fclose(lista);
   return 0;
 }

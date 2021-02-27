776354d309081e4c2d5fab4b849c7a5507c64714
Danib0y97 <66141645+Danib0y97@users.noreply.github.com>
2021-02-27 10:56:55 -0300

    Programa rodando certo
    Quase terminando, apenas corrigindo alguns problemas

--- pseud/trab/main.c
+++ pseud/trab/main.c
@@ -10,16 +10,17 @@
 
 int main(){
 
-  /*Contadores de palavras*/
-  int i = 0, j = 0, c , x , n = 0, score = 0, decisao, tam_resposta, tam_anagrama;
+  /*Contadores*/
+  int i = 0, j = 0, c = 0, x = 0, score = 0, decisao, tam_resposta, tam_anagrama;
   /*Arquivo*/
   FILE *lista;  
   /*Palavras da lista*/
-  char palavra[20], palavraslista[20], anagrama[20], r[20], resposta[20], * comparar; //palavras_lidas[0];
+  char palavra[TAM_LISTA], palavraslista[TAM_LISTA], anagrama[TAM_LISTA], 
+  r[TAM_LISTA], resposta[TAM_LISTA], palavras_lidas[TAM_LISTA] = {0}, * comparar;
   /*Sorteia a palavra aleatória da lista*/
-  int sorteio = 0, nivel;
+  int sorteio = 0;
  
-  printf("//////////// Seja bem-vindo a ANAGRAMA! ////////////\n\n");
+  printf("////////// Seja bem-vindo a ANAGRAMA! //////////\n\n");
 
   do{
     printf("\n1.Iniciar\n2.Como jogar?\n3.Sair\n\n");
@@ -31,7 +32,7 @@ int main(){
         printf("\nPronto?! Vamos lá!\n\n");
         break;
       case 2:
-        printf("\nO objetivo desse jogo e formar uma palavra com as letras que serao apresentadas na tela.\n");
+        printf("\nO objetivo desse jogo e formar um anagrama com as letras que serao apresentadas na tela.\nDigite RANDOM para gerar uma nova palavra e FIM para sair do jogo!\n\nDivirta-se!\n");
         printf("\n1.Voltar\n2.Sair\n\n");
         scanf("%d", &decisao);
         if(decisao == 1){
@@ -54,22 +55,22 @@ int main(){
     break;
   } while(1);
   
-  while(1){
+  
   /*Abrindo o arquivo e lendo as palavras*/
-
-    lista = fopen("facil.txt","r");
+    
+    lista = fopen("palavras.txt","r");
     if(lista == NULL) {
       printf("O arquivo nao pode ser aberto.\n");
       exit(1);
-    }
-    rewind(lista);
+    }  
+
+
+  while(1){  
     
     /*Sorteando palavras da lista*/
-
+    
     while(fscanf(lista, "%s", palavra) != EOF){
-      //strcpy(palavraslista, palavra);
       rewind(lista);
-
       srand(time(NULL));
       sorteio = rand() % TAM_LISTA;
 
@@ -80,6 +81,7 @@ int main(){
         }
       }
       break;
+      fclose(lista);
     }
     printf("%s\n", palavra);
 
@@ -100,23 +102,24 @@ int main(){
             c++;
           }
           else {
-            x = rand() % strlen(palavra);
+            x = rand()%strlen(palavra);
             c = 0;
             j = -1;
           }
-          if(c == i) {
+          if(c == i) { 
             anagrama[i] = palavra[x];
             r[i] = x;
           }
         }  
       }
-    }
+    }  
     printf("%s\n\n", anagrama);
-
+    
 
   /*//////////Resposta//////////*/
     
     scanf("%s", resposta);
+
 
   /*Tranformando em letra maiúscula caso precise*/
     
@@ -135,12 +138,11 @@ int main(){
   /*Condição de parada*/
     
     if(strcmp(resposta,"FIM")==0){
-      printf("Obrigada por jogar! Até mais!");
+      printf("Obrigada por jogar! Ate mais!");
       return 0;
     }
 
-   
-  //////////////////* Não repetindo a palavra *////////////////////////
+  /////////////////* Não repetindo a palavra *////////////////////////
 
    /* for(i = 0; i < strlen(palavra); i++){
          n  =  palavras_lidas[0];
@@ -153,30 +155,32 @@ int main(){
     
     tam_anagrama = strlen(palavra);
     if(tam_resposta != tam_anagrama){
-      printf("\nNao e um anagrama! Tente novamente!\n");
+      printf("Nao e um anagrama!\n\n");
+      printf("SCORE: %d\n\n", score);
       continue;
     }
     
-    rewind(lista); /////NAO TIRAR!
+    rewind(lista);
     while(fgets(palavraslista, sizeof(palavraslista), lista)){  
-        comparar = strstr(palavraslista, resposta);
-        if (comparar){
-          break;
-        }
-      }
+      comparar = strstr(palavraslista, resposta);
       if (comparar){
-          score = score + 10;
-          printf("É anagrama!\n\n");
-          printf("SCORE: %d\n\n", score);
-          scanf("%d", &decisao);
-          continue;
-        }
-        else{
-          printf("Nao e anagrama!\n\n");
-          printf("SCORE: %d\n\n", score);
-          continue;
-        }     
-      }      
-  fclose(lista);
+        break;
+        
+      }
+    }
+    if (comparar){
+      score = score + 10;
+      printf("E anagrama!\n\n");
+      printf("SCORE: %d\n\n", score);
+      continue;
+      }
+      else{
+        printf("Nao e anagrama!\n\n");
+        printf("SCORE: %d\n\n", score);
+        continue;
+      }  
+    }  
+    
+    fclose(lista);  
   return 0;
 }
